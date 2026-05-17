1db8f68ba7a788ab05492201946df62797841f60
jmmcmx <josemmaciascontreras@icloud.com>
2026-05-17 00:00:02 -0600

    backup: 2026-05-17 00:00:01

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -19,5 +19,17 @@ module.exports = {
       out_file: 'logs/out.log',
       merge_logs: true
     }
+    ,{
+      name: 'xcien-telegram-bot',
+      script: 'backend/agents/telegram_agent_bot.py',
+      interpreter: '.venv/bin/python3',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      error_file: 'logs/telegram-err.log',
+      out_file: 'logs/telegram-out.log',
+      merge_logs: true
+    }
   ]
 };
